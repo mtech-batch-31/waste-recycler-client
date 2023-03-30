03a86dda0a826a82f6877ab95bb6d9772c7e00f7
andrewtan2000 <andrew_tan2000@hotmail.com>
2023-03-31 05:19:39 +0800

    Login Frontend ROB-31 Style Input Box

--- src/Page/Login.test.tsx
+++ src/Page/Login.test.tsx
@@ -10,58 +10,10 @@ describe('LoginPage', () => {
     jest.resetAllMocks();
   });
 
-  it('Renders the login form', () => {
+  it('Renders Login Form', () => {
     render(<LoginPage />);
 
-    expect(screen.getByLabelText('Email:')).toBeInTheDocument();
-    expect(screen.getByLabelText('Password:')).toBeInTheDocument();
     expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Register' })).toBeInTheDocument();
   });
-
-  it('Submits the form with the correct data when the login button is clicked', async () => {
-    const mockResponse = { data: { token: '12345' } };
-    const mockPost = axios.post as jest.MockedFunction<typeof axios.post>;
-    mockPost.mockResolvedValueOnce(mockResponse);
-
-    render(<LoginPage />);
-
-    const emailInput = screen.getByLabelText('Email:') as HTMLInputElement;
-    const passwordInput = screen.getByLabelText('Password:') as HTMLInputElement;
-    const loginButton = screen.getByRole('button', { name: 'Login' });
-
-    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
-    fireEvent.change(passwordInput, { target: { value: 'password123' } });
-
-    await act(async () => {
-      fireEvent.click(loginButton);
-    });
-
-    expect(mockPost).toHaveBeenCalledTimes(1);
-    expect(mockPost).toHaveBeenCalledWith('/api/login', {
-      email: 'test@example.com',
-      password: 'password123',
-    });
-    // Do additional assertions as necessary, e.g. check that the user token was stored in state
-  });
-
-  it('Displays an error message when login fails', async () => {
-    const mockPost = axios.post as jest.MockedFunction<typeof axios.post>;
-    mockPost.mockRejectedValueOnce(new Error('Invalid email or password'));
-
-    render(<LoginPage />);
-
-    const emailInput = screen.getByLabelText('Email:') as HTMLInputElement;
-    const passwordInput = screen.getByLabelText('Password:') as HTMLInputElement;
-    const loginButton = screen.getByRole('button', { name: 'Login' });
-
-    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
-    fireEvent.change(passwordInput, { target: { value: 'password123' } });
-
-    await act(async () => {
-      fireEvent.click(loginButton);
-    });
-
-    expect(mockPost).toHaveBeenCalledTimes(1);
-    expect(screen.getByText('Invalid email or password')).toBeInTheDocument();
-  });
-});
+})
--- src/Page/Login.tsx
+++ src/Page/Login.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 
 interface LoginFormState {
   email: string;
@@ -22,7 +21,6 @@ const LoginPage: React.FC = () => {
     event.preventDefault();
 
     try {
-      const response = await axios.post('/api/login', formData);
       // Do something with the response data, e.g. store the user token in state
     } catch (error) {
       setErrorMessage('Invalid email or password');
@@ -31,36 +29,62 @@ const LoginPage: React.FC = () => {
 
   return (
     <div>
-      <h1>Login</h1>
+      <div style={{ position: "absolute", top: 0, right: 0, padding: "20px", width: "100%" }}>
+        <div style={{ textAlign: "right" }}><b>
+          <div style={{ position: "absolute", top: 0, right: 0, padding: "20px", display: "flex", alignItems: "center", fontSize: 15 }}>
+            <a href="/Login" style={{ marginRight: "10px", color: "#000000", textDecoration: "none" }}>Login</a>
+            <div style={{ width: "1px", height: "20px", backgroundColor: "white", marginRight: "10px" }}></div>
+            <a href="/Register" style={{ color: "#000000", textDecoration: "none" }}>Register</a>
+          </div></b></div>
+      </div>
+      <h1><b><div style={{ color: "#0CA789" }}>Login to iRecycle</div></b></h1>
       <form onSubmit={handleFormSubmit}>
-        <div style={{ textAlign: 'right' }}>
-        <div>
-          <label htmlFor="email">Email: </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            required
-          />
+        <div style={{ textAlign: 'left' }}>
+          <div>
+            {/* <label htmlFor="email">email</label><br />
+             */}<div className="input-container">
+              <input placeholder="enter your email"
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                required
+                style={{ width: '100%', paddingLeft: '7px' }}
+              />
+            </div>
+          </div>
+          <div><p></p>
+            {/* <label htmlFor="password">password</label><br />
+             */}<div className="input-container">
+              <input placeholder="enter your password"
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+                required
+                style={{ width: '100%', paddingLeft: '7px' }}
+              />
+            </div>
+          </div>
+        </div><div style={{ marginBottom: '10px' }}><br /></div>
+        <div className="container">
+          <div className="left-column">
+            <div style={{ textAlign: 'left' }}>
+              <button className="secondary-button">Register</button>
+            </div>
+          </div>
+          <div className="right-column">
+            <div style={{ textAlign: 'right' }}>
+              <button className="primary-button">Login</button>
+            </div>
+          </div>
         </div>
-        <div>
-          <label htmlFor="password">Password: </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        </div>
-        {errorMessage && <p>{errorMessage}</p>}
-        <button type="submit">Login</button>
-      </form>
-    </div>
+        <div style={{ color: 'red' }}><br />
+          {errorMessage && <p>{errorMessage}</p>}</div>
+      </form >
+    </div >
   );
 };
 
