37d3b3b5a3e2c4b959c52bb6e6432080712cd879
andrewtan2000 <andrew_tan2000@hotmail.com>
2023-04-07 20:01:08 +0800

    ROB-31 Session storage of access-token

--- src/Page/Login.test.tsx
+++ src/Page/Login.test.tsx
@@ -1,48 +1,10 @@
-// LoginBootStrap.test.tsx
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 import Login from './Login';
 
-describe('LoginBootStrap component', () => {
-    beforeEach(() => {
-        render(<Login />);
-    });
-
-    it('renders the login form', () => {
-        const emailInput = screen.getByPlaceholderText('Enter your email');
-        const passwordInput = screen.getByPlaceholderText('Enter your password');
-        const loginButton = screen.getByText('Login');
-        const registerButton = screen.getByText('Register');
-
-        expect(emailInput).toBeInTheDocument();
-        expect(passwordInput).toBeInTheDocument();
-        expect(loginButton).toBeInTheDocument();
-        expect(registerButton).toBeInTheDocument();
-    });
-
-    it('updates the email and password fields', () => {
-        const emailInput = screen.getByPlaceholderText('Enter your email') as HTMLInputElement;
-        const passwordInput = screen.getByPlaceholderText('Enter your password') as HTMLInputElement;
-
-        userEvent.type(emailInput, 'test@example.com');
-        userEvent.type(passwordInput, 'testPassword');
-
-        expect(emailInput.value).toBe('test@example.com');
-        expect(passwordInput.value).toBe('testPassword');
-    });
-
-    it('shows an error message when form submission fails', async () => {
-        const emailInput = screen.getByPlaceholderText('Enter your email') as HTMLInputElement;
-        const passwordInput = screen.getByPlaceholderText('Enter your password') as HTMLInputElement;
-        const loginButton = screen.getByText('Login');
-
-        userEvent.type(emailInput, 'test@example.com');
-        userEvent.type(passwordInput, 'testPassword');
-
-        fireEvent.click(loginButton);
-
-        const errorMessage = await screen.findByText('Invalid email or password');
-        expect(errorMessage).toBeInTheDocument();
-    });
+test('renders email and password form inputs', () => {
+    render(<Login />);
+    const emailInput = screen.getByLabelText(/email address/i);
+    expect(emailInput).toBeInTheDocument();
+    const passwordInput = screen.getByLabelText(/password/i);
+    expect(passwordInput).toBeInTheDocument();
 });
--- src/Page/Login.tsx
+++ src/Page/Login.tsx
@@ -1,21 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Form, Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './Login.css';
 import axios from 'axios';
+// import MockAdapter from 'axios-mock-adapter'; // import the mocking library
+import Cookies from 'js-cookie';
 
 interface LoginFormState {
     email: string;
     password: string;
 }
 
-const LoginBootStrap: React.FC = () => {
-    const [formData, setFormData] = useState<LoginFormState>({
+interface ResponseData {
+    message: string;
+    accessToken: string;
+    refreshToken: string;
+}
+
+const Login: React.FC = () => {
+    const initialFormData: LoginFormState = {
         email: '',
         password: '',
-    });
+    };
+
+    const [formData, setFormData] = useState<LoginFormState>(initialFormData);
+    const [responseData, setResponseData] = useState<ResponseData | null>(null);
     const [errorMessage, setErrorMessage] = useState<string>('');
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+    useEffect(() => {
+        const token = Cookies.get('accessToken');
+        if (token) {
+            axios
+                .get('http://localhost:8080/api/v1/auth/check-token', {
+                    headers: { Authorization: `Bearer ${token}` },
+                })
+                .then(() => setIsLoggedIn(true))
+                .catch(() => Cookies.remove('accessToken'));
+        }
+    }, []);
+
+    const removeToken = () => {
+        Cookies.remove('access_token');
+    };
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
@@ -24,12 +52,35 @@ const LoginBootStrap: React.FC = () => {
 
     const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        // Clear the responseData and errorMessage
+        setResponseData(null);
+        setErrorMessage('');
+
+        // Create a new instance of the mocking library and set it up to intercept the axios.post call
+        // const mock = new MockAdapter(axios);
+        // mock.onPost('http://localhost:8080/api/v1/auth/login').reply(200, {
+        //     returnCode: '00',
+        //     message: 'The request has been successfully processed',
+        //     accessToken:
+        //         'eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJzY2dsd3NqQDE2My5jb20iLCJpYXQiOjE2ODA2NzM0MzAsImV4cCI6MTY4MDY3NDMzMH0.sCts66QMeHgsk2daV2qUfCy4bZee625QY73jGsy7uQLo0orNo6W7BP2gijFHHBrg',
+        //     refreshToken: 'd7c039c1-2bd2-476b-a2fe-da0367d4c94f',
+        // });
 
         try {
-            const response = await axios.post('/api/login', formData);
-            // Do something with the response data, e.g. store the user token in state
+            const response = await axios.post('http://localhost:8080/api/v1/auth/login', {
+                email: formData.email, // Use the email value for the userName parameter
+                password: formData.password
+            }, {
+                withCredentials: true, // Add the withCredentials flag
+            });
+
+            // Retrieve the token from the response
+            const token = response.data.accessToken;
+            Cookies.set('access_token', token);
+            setResponseData(response.data);
         } catch (error) {
             setErrorMessage('Invalid email or password');
+            removeToken();
         }
     };
 
@@ -61,6 +112,7 @@ const LoginBootStrap: React.FC = () => {
                                 required
                             />
                         </Form.Group>
+                        {responseData && <div className="text-success mr-2 d-inline-block">{responseData.message}</div>}
                         {errorMessage && <div className="text-danger mr-2 d-inline-block">{errorMessage}</div>}<br />
                         <Row className="d-flex justify-content-between align-items-center">
                             <Col>
@@ -77,4 +129,4 @@ const LoginBootStrap: React.FC = () => {
     );
 };
 
-export default LoginBootStrap;
+export default Login;
